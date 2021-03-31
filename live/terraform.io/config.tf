51cc0aaef51a306faa5977e19fdbc8287bbdd43e
mliu-mettle <michael.liu@mettle.co.uk>
2021-03-31 08:48:54 +0100

    Turn on vcs integration.

--- live/terraform.io/config.tf
+++ live/terraform.io/config.tf
@@ -9,6 +9,8 @@ terraform {
   }
 }
 
-provider "tfe" {}
+provider "tfe" {
+  token = "XNVJVOYjNy3RyA.atlasv1.MtW8KfrBMmj0eB0jyyFzZebr5BRGbnAfjqqhoXkAUa3ByfyL2qdByx0jnh3xzOpu49g"
+}
 
 provider "github" {}
