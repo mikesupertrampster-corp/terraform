a1216973b6f7695713508fd4d0f64326e19b7ace
mikesupertrampster <mikesupertrampster@gmail.com>
2022-03-18 15:33:26 +0700

    Provide descriptions to repositories.

--- mikesupertrampster-corp/github.com/main.tf
+++ mikesupertrampster-corp/github.com/main.tf
@@ -9,26 +9,27 @@ provider "github" {
 locals {
   repositories = {
     standalone = {
-      gangway-kube-conf = { topics = ["golang", "k8s", "idp"] }
-      nixos             = { topics = ["nixos", "nix", "linux", "os"] }
-      packer            = { topics = ["packer", "hashicorp"] }
-      terraform         = { topics = ["terraform", "github", "k8s", "hashicorp"], is_template = true }
-      trader            = { topics = ["finance", "golang", "grafana", "fluxdb", "prometheus"] }
+      gangway-kube-conf = { topics = ["golang", "k8s", "idp"], description = "Commandline to obtain kube-configuration via gangway" }
+      nixos             = { topics = ["nixos", "nix", "linux", "os"], description = "NixOS and Home-Manager configurations" }
+      packer            = { topics = ["packer", "hashicorp"], description = "Building images via Hashicorp Packer" }
+      terraform         = { topics = ["terraform", "github", "k8s", "hashicorp"], is_template = true, description = "Terraform for Everything" }
+      trader            = { topics = ["finance", "golang", "grafana", "fluxdb", "prometheus"], description = "Trader platform using Prometheus & Grafana" }
+      yubikey           = { topics = ["security", "yubikey"], description = "Set up guide for Yubikey (GPG + SSH)" }
     }
 
     terraform-module = {
-      github-repository = { topics = ["terraform", "module", "hashicorp", "terraform-registry", "github"], is_template = true }
-      tfe-management    = { topics = ["terraform", "module", "hashicorp", "terraform-registry", "terraform-cloud"] }
+      github-repository = { topics = ["terraform", "module", "hashicorp", "terraform-registry", "github"], is_template = true, description = "Terraform module for creating creating Github repositories" }
+      tfe-management    = { topics = ["terraform", "module", "hashicorp", "terraform-registry", "terraform-cloud"], description = "Terraform modules for setting up TF Cloud" }
     }
 
     docker-builds = {
-      simple-json-server = { topics = ["docker", "golang", "json", "http"], is_template = true }
+      simple-json-server = { topics = ["docker", "golang", "json", "http"], is_template = true, description = "Dynamically serves JSON from files" }
     }
 
     algo = {
-      api             = { topics = ["golang", "json", "http", "finance"], is_template = true }
-      batch-collector = { topics = ["golang", "json", "http", "finance"] }
-      feeder          = { topics = ["golang", "json", "http", "finance", "prometheus"] }
+      api             = { topics = ["golang", "json", "http", "finance"], is_template = true, description = "Interacts with the following company information services" }
+      batch-collector = { topics = ["golang", "json", "http", "finance"], description = "Collects company information via service API" }
+      feeder          = { topics = ["golang", "json", "http", "finance", "prometheus"], description = "Sends company information to Prometheus database" }
     }
   }
 
@@ -45,6 +46,7 @@ module "repositories" {
   source                   = "app.terraform.io/mikesupertrampster-corp/github-repository/module"
   version                  = "1.0.5"
   name                     = each.key
+  description              = lookup(each.value, "description", null)
   topics                   = each.value["topics"]
   visibility               = lookup(each.value, "visibility", "public")
   required_status_checks   = { "gitleaks" = true }
