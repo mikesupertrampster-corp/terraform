cef145f05c101117c29e2259f744bd8e51dacbd7
mikesupertrampster <mikesupertrampster@gmail.com>
2022-04-08 01:59:13 +0700

    Bootstrap datadog terraform.

--- mikesupertrampster-corp/datadog.com/main.tf
+++ mikesupertrampster-corp/datadog.com/main.tf
@@ -5,3 +5,20 @@ terraform {
     }
   }
 }
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+}
+
+resource "datadog_api_key" "key" {
+  name = "terraform"
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+}
+
+resource "datadog_application_key" "key" {
+  name = "terraform"
+}
